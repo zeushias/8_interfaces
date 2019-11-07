247b187bcd36d25ab6f20fa2ddc039f7d8c58fb3
Zeushias <zeushias@gmail.com>
2019-11-07 14:48:02 +0100

    la classe produit

---
+++ src/metier/Produit.java
@@ -0,0 +1,39 @@
+package metier;
+
+public class Produit {
+
+	private int id;
+	private String libelle;
+	private double prix;
+	
+	public Produit(int id, String libelle, double prix) {
+		super();
+		this.id = id;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	@Override
+	public String toString() {
+		return "Produit [id=" + id + ", libelle=" + libelle + ", prix=" + prix + "]";
+	}	
+}
