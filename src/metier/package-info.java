8c85bb912c64368f5e01fdb1553224d0802e615c
Zeushias <zeushias@gmail.com>
2019-11-07 14:42:21 +0100

    lanceur

--- src/metier/package-info.java
+++ src/metier/package-info.java
@@ -1,8 +1,5 @@
 /**
- * 
- */
-/**
- * @author zeuhias
+ * @author zeushias
  *
  */
 package metier;
---
+++ src/presentation/Lanceur.java
@@ -0,0 +1,14 @@
+package presentation;
+
+public class Lanceur {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Interface");
+	}
+
+}
